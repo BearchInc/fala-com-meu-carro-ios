13c043bbcca75643e1b2ba8b8658a07675335efa
Heck/Ygor <heckfer@gmail.com>
2015-09-10 11:15:29 -0300

    [#00] Fix routes

--- FalaComMeuCarro/Services/Router.swift
+++ FalaComMeuCarro/Services/Router.swift
@@ -26,9 +26,9 @@ enum Router: URLRequestConvertible {
 	var path: String {
 		switch self {
 		case .CreatePost(let carPlate, let message, let userId, let userName):
-			return "/posts/"
+			return "/posts"
 		case .ListAllPosts():
-			return "/posts/"
+			return "/posts"
 		case .ListPostByCarPlate(let carplate):
 			return "/posts/\(carplate)"
         case .FlagPost(let postId):
